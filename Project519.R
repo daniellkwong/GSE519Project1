588d9ffc3f3544d12344120157df3ac43ff873f0
Daniel <danielkwong04@gmail.com>
2025-10-15 16:43:29 -0700

    update

--- Project519.R
+++ Project519.R
@@ -1,19 +1,15 @@
 options(scipen=999)
 library(readxl)
-setwd("/Users/danielkwong/Documents/GSE519")
+setwd("/Users/danielkwong/Documents/GitHub/GSE519Project")
 
 #Import data
 myData <- read.csv("train.csv")
 
 
 # Rename columns that start with numbers
-names(train)[names(train) == "1stFlrSF"] <- "X1stFlrSF"
-names(train)[names(train) == "2ndFlrSF"] <- "X2ndFlrSF"
-names(train)[names(train) == "3SsnPorch"] <- "X3SsnPorch"
-
-names(test)[names(test) == "1stFlrSF"] <- "X1stFlrSF"
-names(test)[names(test) == "2ndFlrSF"] <- "X2ndFlrSF"
-names(test)[names(test) == "3SsnPorch"] <- "X3SsnPorch"
+names(myData)[names(myData) == "1stFlrSF"] <- "X1stFlrSF"
+names(myData)[names(myData) == "2ndFlrSF"] <- "X2ndFlrSF"
+names(myData)[names(myData) == "3SsnPorch"] <- "X3SsnPorch"
 
 #Convert categorical variables into dummy variables
 install.packages("fastDummies")
@@ -26,8 +22,6 @@ cat_vars <- c("MSZoning", "LotShape", "LandContour", "LotConfig", "LandSlope", "
               "Electrical", "KitchenQual", "Functional", "FireplaceQu", "GarageType", 
               "GarageFinish", "GarageQual", "GarageCond", "PavedDrive")
 
-train[cat_vars] <- lapply(train[cat_vars], as.factor)
-test[cat_vars]  <- lapply(test[cat_vars],  as.factor)
+myData[cat_vars] <- lapply(myData[cat_vars], as.factor)
 
-train_with_dummies <- dummy_cols(train, select_columns = cat_vars, remove_selected_columns = TRUE)
-test_with_dummies  <- dummy_cols(test,  select_columns = cat_vars, remove_selected_columns = TRUE)
+myData_with_dummies <- dummy_cols(myData, select_columns = cat_vars, remove_selected_columns = TRUE)
